ecccf9a8d79f11e3a5d453d331303b898724ffa0
Denis Lipatov <nutela113@yandex.ru>
2025-02-08 16:09:53 +0300

    Moved login methods from MainPage to BasePage

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.common import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
@@ -7,6 +5,8 @@ import math
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -17,6 +17,13 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
